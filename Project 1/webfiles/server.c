132a7ae5dfb1f972a467c4573521197f21f15279
Gaurav13274 <gaurav21776@gmail.com>
2016-08-19 05:59:06 +0530

    Version 2 ready

--- Project 1/webfiles/server.c
+++ Project 1/webfiles/server.c
@@ -2,7 +2,7 @@
 * @Author: gaurav
 * @Date:   2016-08-13 21:50:53
 * @Last Modified by:   gaurav
-* @Last Modified time: 2016-08-18 01:10:53
+* @Last Modified time: 2016-08-19 05:29:52
 */
 
 #include <stdio.h>
@@ -13,15 +13,46 @@
 #include <string.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <time.h>
 #define BACKLOG 10
 #define BUFFER_SIZE 1024
+#define SERVER "project1Server"
+char *badFile = "File not found";
+char *okMessage = "Success";
+char *badRequestMethod = "Unsupported method";
+char *badRequest = "Malformed request";
+char *badURIRequest = "Too Long";
+char *serverError = "HTTP/1.0 500 Unexpected error\r\n";
 
-char *badFile = "HTTP/1.0 404 Not Found\r\n";
-char *okMessage = "HTTP/1.0 200 OK\r\n";
-char *badRequestProtocol = "HTTP/1.0 400 Bad Request\r\n";
-char *badRequestMethod = "HTTP/1.0 501 Not Implemented\r\n";
-char *badRequest = "HTTP/1.0 400 Bad Request\r\n";
-char *badURIRequest = "HTTP/1.0 414 Request-URI Too Long\r\n";
+// Thanks to http://stackoverflow.com/questions/4553012/checking-if-a-file-is-a-directory-or-just-a-file
+int isDirectory(const char *path) {
+   struct stat statbuf;
+   if (stat(path, &statbuf) != 0)
+       return 0;
+   return S_ISDIR(statbuf.st_mode);
+}
+
+void sendStatusMessage(int socket, char* protocol,int status, char *reason, char *comment) {
+  char error[BUFFER_SIZE];
+  bzero(error,sizeof(error));
+  // donot send Content length in ok message
+  if (status==200){
+  	sprintf(error,"%s %d %s\r\n",protocol,status,reason);	
+  }
+  else{
+  	sprintf(error,"%s %d %s\r\nContent-type: text/txt\r\nContent-Length: %d\r\n\r\n%s",protocol,status,reason,(int)strlen(comment),comment);
+  }
+  if(send(socket,error,strlen(error),0)!=strlen(error)){
+  		send(socket,serverError,strlen(serverError),0);
+  		close(socket);
+  		exit(1);
+  }
+  return;
+}
 
 char* getContentType(char* filename){
 	if(strstr(filename,".pdf")){
@@ -43,46 +74,124 @@ char* getContentType(char* filename){
 		return "application/octet-stream";
 	}
 }
-void handleGet(int newHandle,char* uri){
+
+void showDir(char *filepath,int newHandle,char *protocol,char* connectionType,char *uri){
+	DIR *dir;
+	char directory[5*BUFFER_SIZE];
+	bzero(directory,sizeof(directory));
+	struct dirent *ent;
+	char * ref = "<HEAD><TITLE>%s</TITLE></HEAD><BODY> %s</BODY>";
+	if ((dir = opendir (filepath)) != NULL) {
+	  /* print all the files and directories within directory */
+	  while ((ent = readdir (dir)) != NULL) {
+
+	  	if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0)
+                continue;
+	  	
+		char tmp[BUFFER_SIZE];
+		char res[BUFFER_SIZE];
+		bzero(tmp,sizeof(tmp));
+		bzero(res,sizeof(res));
+		strcpy(res,uri);
+		strcat(res,"/");
+		printf("RES%s\n", res);
+		printf("%s\n", ent->d_name);
+		sprintf(tmp,"<a class=\"icon file \"  href=\"%s\">%s</a><br/>",strcat(res,ent->d_name),ent->d_name);
+	  	printf("%s\n",tmp);
+	  	if (strlen(directory)+strlen(ent->d_name)<5*BUFFER_SIZE){
+	  		strcat(directory,tmp);
+	  	}
+	  	// can't handle directory greater than buffer size
+	  	else{
+	  		sendStatusMessage(newHandle,protocol,500,"Internal Server Error","Unexpected error");
+			close(newHandle);
+			exit(1);
+	  	}
+	  }
+	  printf("%s\n",directory );
+	  closedir (dir);
+	} 
+	else {
+	  /* could not open directory */
+	  sendStatusMessage(newHandle, protocol,404,"Not Found",badFile);
+	  return;
+	}
+	printf("%s\n",directory);
+	sendStatusMessage(newHandle,protocol,200,"OK",okMessage);
+	time_t now = time(NULL);
+  	char date[200];
+  	strftime(date, sizeof(date),"%a, %d %b %Y %H:%M:%S GMT", gmtime(&now));
+	char buffer[2*BUFFER_SIZE];
+	bzero(buffer,sizeof(buffer));
+	sprintf(buffer,"Server: %s\r\nDate: %s\r\nContent-Length: %d\r\nContent-Type: %s\r\nConnection: %s\r\n\r\n",SERVER,date,(int)strlen(directory),"text/html",connectionType);
+	strcat(buffer,directory);
+		if(send(newHandle,buffer,strlen(buffer),0)!=strlen(buffer)){
+			sendStatusMessage(newHandle,protocol,500,"Internal Server Error","Unexpected error");
+			close(newHandle);
+			exit(1);
+		}
+	return;	
+}
+void handleGet(int newHandle,char* uri,char* protocol,char * connectionType,char *root){
 	//printf("%s\n",uri );
 	char filepath[5*BUFFER_SIZE];
-	strcpy(filepath,getenv("PWD"));
+	strcpy(filepath,root);
 	FILE *fp;
 	char buffer[BUFFER_SIZE];
-
-	if (!strcmp(uri,"/")||!strcmp(uri,"index.html")){
-		uri = "/index.html";
-	}
+	
 	// getting file path to send
 	strcpy(&filepath[strlen(filepath)],uri);
-	// sprintf(filename,"%s",uri+1);
-	printf("FILE %s \nURI:%s\n",filepath,uri );
-	if((fp = fopen(filepath,"rb"))==NULL){
+	printf("FILEBEFORE%s\n",filepath );
+	if (isDirectory(filepath)){
+		// show index.html
+		if (filepath[strlen(filepath)-1]=='/')
+			strcat(filepath,"/index.html");
+		// show directory
+		else{
+			showDir(filepath,newHandle, protocol,connectionType,uri);
+			return;
+		}
+		printf("URI-->%s\n", filepath);
+	}
+	
+	printf("FILE%sROOT%s\n",filepath,root );
+	if((fp = fopen(filepath,"r"))==NULL){
 		// file not found error
-		send(newHandle,badFile,BUFFER_SIZE,0);
+		sendStatusMessage(newHandle, protocol,404,"Not Found",badFile);
 		return;
 	}
 	else{
 		// file found
-		send(newHandle,okMessage,strlen(okMessage),0);
+		sendStatusMessage(newHandle,protocol,200,"OK",okMessage);
 		// preparing header
-		char* contentType = getContentType(uri);
+		char* contentType = getContentType(filepath);
 		// content length
 		fseek(fp, 0L, SEEK_END);
 		int contentLength = ftell(fp);
 		fseek(fp, 0L, SEEK_SET);
 		bzero(buffer,sizeof(buffer));
-		sprintf(buffer,"Content-Length: %d\r\nContent-Type: %s\r\n\r\n",contentLength,contentType);
-		send(newHandle,buffer,strlen(buffer),0);
+		
+		time_t now = time(NULL);
+  		char date[200];
+  		strftime(date, sizeof(date),"%a, %d %b %Y %H:%M:%S GMT", gmtime(&now));
+
+
+		sprintf(buffer,"Server: %s\r\nDate: %s\r\nContent-Length: %d\r\nContent-Type: %s \r\nConnection: %s\r\n\r\n",SERVER,date,contentLength,contentType,connectionType);
+		if(send(newHandle,buffer,strlen(buffer),0)!=strlen(buffer)){
+			sendStatusMessage(newHandle,protocol,500,"Internal Server Error","Unexpected error");
+			close(newHandle);
+			exit(1);
+		}
+
 		int bytesRead;
+		bzero(buffer,sizeof(buffer));
 		// sending file
 		while((bytesRead = fread(buffer,1,sizeof(buffer),fp))>0){
-			// if reading failed shut down server and close the connection
-			if(ferror(fp)) { printf("Fatal Error\n"); close(newHandle); exit(1);}
-
-			if (send(newHandle,buffer,bytesRead,0)<0){
-				perror("Error: Send Failed!!!");
-				return;
+			// if reading failed shut down server and close the connection sending error message
+			if (send(newHandle,buffer,bytesRead,0)!=bytesRead || ferror(fp)){
+				sendStatusMessage(newHandle,protocol,500,"Internal Server Error","Unexpected error");
+				close(newHandle); 
+				exit(1);
 			}
 			bzero(buffer,sizeof(buffer));
 		}
@@ -90,68 +199,113 @@ void handleGet(int newHandle,char* uri){
 	}
 }
 
-void handleRequest(int newHandle){
-	int bytesRead;
-	char buffer[BUFFER_SIZE];
-	bzero(buffer,sizeof(buffer));
-	char method[10],uri[BUFFER_SIZE],version[10];
-	// handles request line of max length 4*BUFFER_SIZE
-	char requestLine[4*BUFFER_SIZE+1];
-	bzero(requestLine,sizeof(requestLine));
-	// GIve an error if URI is too long for the server to handle
-	while(strstr(buffer,"\r\n")==NULL){
-		
-		if(strlen(requestLine)== 4*BUFFER_SIZE){
-			send(newHandle,badURIRequest,BUFFER_SIZE,0);
-			return;
-		}
+void handleRequest(int newHandle,char * root){
+	// persistent connection
+	// connection =1 if keep-alive else connection=0
+	int connection = 1;
+	while(1){
+		printf("START\n%d",getpid());
+		int bytesRead;
+		// by default assumed to be http1.1
+		char *protocol = "HTTP/1.1";
+		char buffer[BUFFER_SIZE];
 		bzero(buffer,sizeof(buffer));
-		bytesRead = recv(newHandle,buffer,BUFFER_SIZE,0);
-		// recv failed
-		if(bytesRead==-1){
-			send(newHandle, badRequest,BUFFER_SIZE,0);
-			return;
-		}
-		// client closed connection before sending complete request
-		else if(bytesRead==0){
-			return;
-		}
-		// all good
-		else{
-			strcat(requestLine,buffer);
-		}
-	}
-		// parsing method uri version out of the request message
-		sscanf(requestLine,"%s %s %s",method,uri,version);
-		//bzero(buffer,sizeof(buffer));
-		// Loop till end of request message is found
-		while(strstr(buffer, "\r\n\r\n") == NULL){
+		char method[10],uri[8*BUFFER_SIZE],version[10];
+		// handles request  of max length 8*BUFFER_SIZE
+		char request[8*BUFFER_SIZE+1];
+		bzero(request,sizeof(request));
+		// GIve an error if URI is too long for the server to handle
+		while(strstr(request,"\r\n")==NULL){
+			
+			if(strlen(request)== 4*BUFFER_SIZE){
+				sendStatusMessage(newHandle,protocol,414 ,"Request-URI",badURIRequest);
+				return;
+			}
+			bzero(buffer,sizeof(buffer));
 			bytesRead = recv(newHandle,buffer,BUFFER_SIZE,0);
 			// recv failed
-			if(bytesRead==-1){
-				send(newHandle, badRequest,BUFFER_SIZE,0);
+			if(bytesRead<0){
+				// client closed the connection with error
+				if (errno==ECONNRESET||errno==ECONNABORTED){
+					close(newHandle);
+					exit(1);
+				}
+				sendStatusMessage(newHandle,protocol,400,"Bad Request",badRequest);
 				return;
 			}
 			// client closed connection before sending complete request
 			else if(bytesRead==0){
 				return;
 			}
+			// all good
+			else{
+				strcat(request,buffer);
+			}
 		}
-		// request not GET, reject
-		if (strcmp(method,"GET")){
-			send(newHandle,badRequestMethod,BUFFER_SIZE,0);
-			return;
-		}
-		
-		// protocol version != HTTP1.1
-		if (strcmp(version,"HTTP/1.1")){
-			printf("version\n");
-			send(newHandle,badRequestProtocol,BUFFER_SIZE,0);
-			return;
-		}
-		
-		// all good now handle get request
-		handleGet(newHandle,uri);
+			// parsing method uri version out of the request message
+			sscanf(request,"%s %s %s",method,uri,version);
+			protocol = version;
+			
+			// Loop till end of request message is found
+			while(strstr(buffer, "\r\n\r\n") == NULL){
+				bytesRead = recv(newHandle,buffer,BUFFER_SIZE,0);
+				// recv failed
+				if(bytesRead<0){
+					if (errno==ECONNRESET||errno==ECONNABORTED){
+						close(newHandle);
+						exit(1);
+					}
+					sendStatusMessage(newHandle,protocol,400,"Bad Request",badRequest);
+					return;
+				}
+				// client closed connection before sending complete request
+				else if(bytesRead==0){
+					return;
+				}
+				// all good
+				else{
+					strcat(request,buffer);
+				}
+
+			}
+
+
+
+			// retrieving connection status from request
+			char connectionType[10] = "keep-alive";
+			char *tmp;
+			
+			if((tmp = strstr(request,"Connection:"))!=NULL){
+				char *end = strstr(tmp+12,"\r\n");
+				char* start = tmp+12;
+				tmp =start;	
+				for ( ; tmp<=end; ++tmp) *tmp = tolower(*tmp);
+				if (tmp) 
+					strncpy(connectionType, start, end-start);
+			}
+			printf("%s\n",request );
+			printf("connectionType %s\n", connectionType);
+			// close connection checking
+			if (!strcmp(connectionType,"close"))
+				printf("CONNE%s\n",connectionType );
+				connection = 0;
+
+			// request not GET, reject
+			if (strcmp(method,"GET")){
+				sendStatusMessage(newHandle,protocol,501,"Not Implemented",badRequestMethod);
+				return;
+			}
+			
+			// protocol version != HTTP1.1
+			if (strcmp(version,"HTTP/1.0")&&strcmp(version,"HTTP/1.1")){
+				sendStatusMessage(newHandle,protocol,400,"Bad Request",badRequest);
+				return;
+			}
+			
+			// all good now handle get request
+			handleGet(newHandle,uri,protocol,connectionType,root);
+			
+	}
 }
 
 
@@ -159,12 +313,28 @@ void handleRequest(int newHandle){
 
 int main(int argc, char **argv ) {
 
-	if (argc != 2){
-    	fprintf(stderr, "please give port number");
-    	exit(1);
- 	 }
-
-	int port = strtol(argv[1], NULL, 0);
+	int port = 4000;
+	char * root = getenv("PWD");
+	char c; 
+	//Parsing the command line arguments
+    while ((c = getopt (argc, argv, "p:r:")) != -1)
+        switch (c)
+        {
+            case 'r':
+                root = malloc(strlen(optarg));
+                strcpy(root,optarg);
+                break;
+            case 'p':
+                port = strtol(optarg, NULL, 0);
+                break;
+            case '?':
+                fprintf(stderr,"Wrong arguments given!!!\n");
+                exit(1);
+            default:
+                exit(1);
+        }
+	
+	
 	struct sockaddr_in serverAddr,clientAddr;
 	socklen_t clientAddrSize;  
 	
@@ -187,11 +357,11 @@ int main(int argc, char **argv ) {
 		perror("Error: Listen failed!!!");
 		exit(1);
 	}
-
+	int newHandle;
 	while(1){
 		printf("Listening------->\n");
 		// Get the new port over which file transfer will occur
-		int newHandle = accept(socketHandle,(struct sockaddr *)&clientAddr,&clientAddrSize);
+		newHandle = accept(socketHandle,(struct sockaddr *)&clientAddr,&clientAddrSize);
 		if (newHandle<0){
 			perror("Error: Couldn't accept request from client");
 			exit(1);
@@ -204,21 +374,19 @@ int main(int argc, char **argv ) {
 			close(socketHandle);
 			//handle the request from client
 			
-			while(1){
-				handleRequest(newHandle);
-			}
-			printf("NOT REACHABLE\n");
+			
+		    handleRequest(newHandle,root);
+			printf("SUCCESS EXIT\n");
+			
 			//close the new socket
-			shutdown (newHandle, SHUT_RDWR);
+			//shutdown (newHandle, SHUT_RDWR);
 			close(newHandle);
 			exit(0);
 			}
 		else if(child!=0) {
 			//parent thread
 			// parent thread doesnot need the new socket as child handles it
-			printf("FORK%d\n", child);
-			printf("handle%d\n", newHandle);
-			close(newHandle);
+			//close(newHandle);
 		}
 		else{
 			//error occured
